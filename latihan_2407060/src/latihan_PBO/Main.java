5003403a06bd2f32d8a9ff09dcd0ca2142b5d777
Widia-PBO <lestariririn411@gmail.com>
2026-02-04 09:53:34 +0700

    menambahkan latihan 1 class dan atribut

--- latihan_2407060/src/latihan_PBO/Main.java
+++ latihan_2407060/src/latihan_PBO/Main.java
@@ -1,7 +1,45 @@
 package latihan_PBO;
 
-public class Main {
-public static void main(String[] args) {
-    System.out.println("Praktikum Pertama");
-}    
+
+class Cafe{
+    String nama;
+    String alamat;
+
+
+    // constructor
+    Cafe(String nama, String alamat){
+         this.nama = nama;
+         this.alamat = alamat;
+          System.out.println(nama);
+          System.out.println(alamat);
+    }
 }
+
+public class Main{
+    public static void main(String[] args) {
+
+        Cafe cafe1 = new Cafe("Kopi Kenangan", "Indramayu");
+        Cafe cafe2 = new Cafe("Janji Jiwa",  "Indramayu");
+    }
+}
+
+
+
+
+
+
+    //Cafe cafe1 = new Cafe();
+    //cafe1.nama = "Kopi Kenangan";
+    //cafe1.alamat = "Indramayu";
+
+    //Cafe cafe2 = new Cafe();
+    //cafe2.nama = "Janji Jiwa";
+    //cafe2.alamat = "Indramayu";
+
+    //System.out.println("Nama Cafenya adalah : " + cafe1.nama);
+    //System.out.println("Alamatnya ada di : " + cafe1.alamat);
+
+    //System.out.println("Nama Cafenya adalah : " + cafe2.nama);
+    //System.out.println("Alamatnya ada di : " + cafe2.alamat);
+
+   
