3341519a37edce12558aacd4c664b968db393a88
Widia-PBO <lestariririn411@gmail.com>
2026-04-08 09:10:36 +0700

    Merevisi tugas_3

--- latihan_2407060/src/latihan_PBO/tugas_3/BangunDatar.java
+++ latihan_2407060/src/latihan_PBO/tugas_3/BangunDatar.java
@@ -1,26 +1,20 @@
 package latihan_PBO.tugas_3;
 
 public class BangunDatar {
-    // 1a & 1b. Atribut private panjang dan lebar (Enkapsulasi) 
-    private double panjang;
-    private double lebar;
+    protected double panjang;
+    protected double lebar;
 
-    // 1c. Constructor untuk mengisi nilai 
     public BangunDatar(double panjang, double lebar) {
         this.panjang = panjang;
         this.lebar = lebar;
     }
 
-    // SETTER (Sesuai gaya kodemu sebelumnya)
-    public void setPanjang(double panjang) { this.panjang = panjang; }
-    public void setLebar(double lebar) { this.lebar = lebar; }
+    public double hitungLuas(){
+        return panjang * lebar *2;
+    }
 
-    // GETTER (Sesuai gaya kodemu sebelumnya)
-    public double getPanjang() { return panjang; }
-    public double getLebar() { return lebar; }
-
-    // 1d. Method hitungLuas 
-    public double hitungLuas() {
-        return panjang * lebar;
+    public void tampilkanHasil(){
+        System.out.println("panjang: " + panjang);
+        System.out.println("lebar = " + lebar);
     }
 }
